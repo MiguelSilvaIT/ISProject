90e594b3727c3635b8cf45c0378383d68439b9f3
Bernardo Melo <2211000@my.ipleiria.pt>
2023-12-26 15:21:16 +0000

    Pontos 5 e 6.

--- somiod/App B/Form1.cs
+++ somiod/App B/Form1.cs
@@ -1,35 +1,103 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows.Forms;
 
 namespace App_B
 {
     public partial class Form1 : Form
     {
+       string connectionString =
+       System.Configuration.ConfigurationManager.ConnectionStrings["App_B.Properties.Settings.ConnString"].ConnectionString;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
+            //Create ON
+            var client = new RestClient(@"http://localhost:59352/");
+            var request = new RestRequest(@"api/somiod/", Method.Post);
+            string contentON = "on";
+            request = new RestRequest(@"api/somiod/TVApp/TVApp Container/data/TVAppDataWhenON", Method.Post);
+            request.RequestFormat = DataFormat.Xml;
+            request.AddBody(new ISProject.Models.Data
+            {
+                content = contentON
+            });
+            var response = client.Execute(request);
+            MessageBox.Show("Data Status Code: " + response.StatusCode + "\n" +
+                                           "Content: " + response.Content);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Create OFF
+            var client = new RestClient(@"http://localhost:59352/");
+            var request = new RestRequest(@"api/somiod/", Method.Post);       
+            string contentOFF = "off";
+            request = new RestRequest(@"api/somiod/TVApp/TVApp Container/data/TVAppDataWhenON", Method.Post);
+            request.RequestFormat = DataFormat.Xml;
+            request.AddBody(new ISProject.Models.Data
+            {
+                content = contentOFF
+            });
+            var  response = client.Execute(request);
+            MessageBox.Show("Data Status Code: " + response.StatusCode + "\n" +
+                                           "Content: " + response.Content);
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+            SqlConnection sqlConnection = null;
+
+            string queryString = "SELECT * FROM application WHERE name = 'Comando'";
+
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(queryString, sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    //Create Comando application
+                    var client = new RestClient(@"http://localhost:59352/");
+                    var request = new RestRequest(@"api/somiod/", Method.Post);
+                    request.RequestFormat = DataFormat.Xml;
+                    request.AddBody(new ISProject.Models.Application
+                    {
+                        name = "Comando",
+                    });
+
+                    var response = client.Execute(request);
+                    MessageBox.Show("Application Status Code: " + response.StatusCode + "\n" +
+                                    "Content: " + response.Content);      
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
         }
     }
 }
